d17729afff9d0ac4e814c4958af66c4c5f818ac0
Brandon Schlack <brandonschlack@gmail.com>
2021-05-10 16:48:20 -0700

    Move work to zsh-secret

--- zsh/zsh_custom/work.zsh
+++ zsh/zsh_custom/work.zsh
@@ -2,41 +2,23 @@
 #
 # Projects
 ## Local
-export WORK_ROOT=$HOME/Documents/Development/Projects/Internet_Brands
-fodors=$WORK_ROOT/fodors.com
-fodors_wordpress=$WORK_ROOT/fodors.com-wordpress
+secrets source internet_brands
+
 alias sdf='ssh dev-fodors'
 
-## Remote
-function ibdev_remote() {
-    local sites="/var/www/sites"
-    local server="dev-fodors-web1.internetbrands.com"
-    local dir=""
-    local command="exec \$SHELL -l"
+function jenkins-build() {
+    local site="$1"
+    local branch="$2"
+    local environment="$3"
 
-    while getopts ":s:d:c:" opt; do
-        case ${opt} in
-            s) server=$OPTARG ;;
-            d) dir=$OPTARG ;;
-            c) command=$OPTARG ;;
-        esac
-    done
-    shift $((OPTIND -1))
-
-    if [ -n "$dir" ]; then
-        case $dir in
-            wordpress) dir="${sites}/fodors.com-wordpress" ;;
-            wptheme) dir="${sites}/fodors.com-wordpress/wp-content/themes/fodors" ;;
+    if [ -n "$site" ]; then
+        case $site in
+            main) site=$FODORS_MAIN_JENKINS_JOB ;;
+            wordpress) site=$FODORS_WORDPRESS_JENKINS_JOB ;;
         esac
     else
-        dir="${sites}/fodors.com"
+        site=$FODORS_MAIN_JENKINS_JOB
     fi
 
-    ssh -t $server "cd $dir; clear; $command;"
+    jenkins build "${site}" -p branch_name="${branch}" -p staging_to_build="${environment}" -f -v
 }
-alias rfw="ibdev_remote -d wordpress"
-
-## Jira Plugin Variables
-export JIRA_URL="https://jira.internetbrands.com/"
-export JIRA_NAME="bschlack"
-export JIRA_DEFAULT_ACTION="dashboard"

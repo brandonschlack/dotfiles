ed32036c26af0440bb51f3d75ae12e1f0b18d41d
Brandon Schlack <brandonschlack@gmail.com>
2026-06-16 13:20:30 -0700

    Add bookmarks to yazi

--- home/dot_config/exact_yazi/init.lua
+++ home/dot_config/exact_yazi/init.lua
@@ -1,4 +1,7 @@
-require("git"):setup()
+require("git"):setup {
+	-- Order of status signs showing in the linemode
+	order = 1500,
+}
 
 local pref_by_location = require("pref-by-location")
 pref_by_location:setup({
@@ -9,12 +12,34 @@ pref_by_location:setup({
     }
 })
 
-require("git"):setup {
-	-- Order of status signs showing in the linemode
-	order = 1500,
-}
-
 require("full-border"):setup {
 	-- Available values: ui.Border.PLAIN, ui.Border.ROUNDED
 	type = ui.Border.ROUNDED,
+}
+
+-- Bookmarks for Yamb (Yet Another Mark) plugin
+local bookmarks = {}
+
+local path_sep = package.config:sub(1, 1)
+local home_path = ya.target_family() == "windows" and os.getenv("USERPROFILE") or os.getenv("HOME")
+table.insert(bookmarks, {
+  tag = "home",
+  path = home_path .. path_sep,
+  key = "~"
+})
+table.insert(bookmarks, {
+  tag = "config",
+  path = home_path .. path_sep .. ".config" .. path_sep,
+  key = "c"
+})
+
+require("yamb"):setup {
+  -- Optional, the path ending with path seperator represents folder.
+  bookmarks = bookmarks,
+  -- Optional, recieve notification everytime you jump.
+  jump_notify = true,
+  -- Optional, a string used for randomly generating keys, where the preceding characters have higher priority.
+  keys = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ",
+  -- Optional, the path of bookmarks
+  path = (os.getenv("HOME") .. "/.config/yazi/bookmark"),
 }
